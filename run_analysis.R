a7712e3692e8d7d6023ae2db59d163cec9806d12
Mechaque <mechaque.muiuane@gmail.com>
2017-03-30 12:45:45 -0700

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -1,1 +1,64 @@
+library(plyr)
 
+# Step 1 Merged data
+x_train <- read.table("X_train.txt")
+y_train <- read.table("y_train.txt")
+subject_train <- read.table("subject_train.txt")
+
+x_test <- read.table("X_test.txt")
+y_test <- read.table("y_test.txt")
+subject_test <- read.table("subject_test.txt")
+
+# x data
+x_data <- rbind(x_train, x_test)
+
+# # y data
+y_data <- rbind(y_train, y_test)
+
+# subject data
+subject_data <- rbind(subject_train, subject_test)
+
+# Step 2: mean measurements
+
+features <- read.table("featues.txt")
+
+# get only columns with mean() or std() in their names
+mean_and_std_features <- grep("-(mean|std)\\(\\)", features[, 2])
+
+# subsets
+x_data <- x_data[, mean_and_std_features]
+
+# correct the column names
+names(x_data) <- features[mean_and_std_features, 2]
+
+# Step 3
+# name the activities in the data set
+
+
+activities <- read.table("activity_labels.txt")
+
+# update values with correct activity names
+y_data[, 1] <- activities[y_data[, 1], 2]
+
+# correct column name
+names(y_data) <- "activity"
+
+# Step 4
+Give a meaningfull labels for variables names
+
+
+# correct column name
+names(subject_data) <- "subject"
+
+# bind all the data in a single data set
+all_data <- cbind(x_data, y_data, subject_data)
+
+# Step 5
+# Create a second, independent tidy data set with the average of each variable
+# for each activity and each subject
+###############################################################################
+
+# 66 <- 68 columns but last two (activity & subject)
+averages_data <- ddply(all_data, .(subject, activity), function(x) colMeans(x[, 1:66]))
+
+write.table(averages_data, "averages_data.txt", row.name=FALSE)
